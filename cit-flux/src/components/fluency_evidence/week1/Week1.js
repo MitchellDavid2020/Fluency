f47ec3cd28a218f5fcb5500a1dd0ecf5d7b0b7f1
Dave Mitchell <newdaveeh@gmail.com>
2019-12-04 03:03:07 -0700

    Playing around
    yep at 3am

--- cit-flux/src/components/fluency_evidence/week1/Week1.js
+++ cit-flux/src/components/fluency_evidence/week1/Week1.js
@@ -4,6 +4,12 @@ const week1 = () => (
   <div>
     <h2>Week 1</h2>
     <p>Javascript libraries.</p>
+    <iframe
+      width="560"
+      height="315"
+      src="https://youtu.be/KZAwQVL2goo?t=35"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
   </div>
 );
 
